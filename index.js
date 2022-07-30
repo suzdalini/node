bde369f48faa7214af72c462ed07d7e744bc6df4
suzdalini <suzdalini@gmail.com>
2022-07-30 10:57:25 +0300

    30.07.22

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import jwt from 'jsonwebtoken'
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello world')
+})
+
+app.post('/auth/login', (req, res) => {
+    const token = jwt.sign({
+        email: req.body.email,
+        fullName: 'Вася Пупкин',
+    },
+    'secret123')
+    res.json({
+        success: true,
+        token
+    })
+})
+
+app.listen(4444, (err) => {
+    if(err) {
+        return console.log(err)
+    }
+    console.log('ok')
+})
